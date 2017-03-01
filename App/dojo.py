4f59db7fb7805d59e536549bae3c024a2ad5182b
jimmykkimani@gmail.com <jimmykkimani@gmail.com>
2017-03-01 12:29:09 +0300

    [chore] Add tests for room creation and add person

---
+++ App/dojo.py
@@ -0,0 +1,20 @@
+class Dojo(object):
+    
+    def __init__(self):
+        self.all_rooms = {}
+        self.fellows = {}
+        self.staff = {}
+        self.all_people = {}
+
+    
+    def create_room(self, name, type_room):
+        '''
+        Create new rooms for person(s)
+        '''
+        pass
+
+    def add_person(self, person_name, category, wants_accomodation = 'N'):
+        '''
+        Adds person(person name), category(Fellow/Staff)
+        '''
+        pass
--- App/tests/test_the_dojo.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import os
-
-
-class TestDojo(unittests.TestCase):
-
-    def setup(self):
-        self.test_dojo = Dojo()
