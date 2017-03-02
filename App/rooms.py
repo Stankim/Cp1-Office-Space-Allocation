b1f3a93e28afdfce284270bc66084637e1f9b174
jimmykkimani@gmail.com <jimmykkimani@gmail.com>
2017-03-02 10:58:40 +0300

    [chore] Add  main object and classses for rooms

---
+++ App/rooms.py
@@ -0,0 +1,41 @@
+"""
+File      : rooms.py
+Date      : March, 2017
+Author(s) : Jimmy Kimani <jimmykkimani@gmail.com>
+Desc      : Office Allocator rooms module
+"""
+class Room(object):
+    '''
+    This creates a Room object where the class Office
+    and Livingspace inherit from
+    '''
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Room %s >' % self.name
+
+class Office(Room):
+
+    capacity = 6
+    '''
+    initialization module
+    '''
+
+    def __init__(self, name):
+        super(Office, self).__init__(name)
+
+    def __repr__(self):
+        return '<Office %s >' % self.name
+
+
+class Livingspace(Room):
+
+    capacity = 4
+
+    def __init__(self, name):
+        super(Livingspace, self).__init__(name)
+
+    def __repr__(self):
+        return '<Livingspace %s >' % self.name
+    
