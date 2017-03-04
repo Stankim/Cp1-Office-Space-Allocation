6426cb4b1b084b611eb877d7f4a7cc8a930115ba
jimmykkimani@gmail.com <jimmykkimani@gmail.com>
2017-03-04 11:45:18 +0300

    [fix] Add changes

--- App/dojo.py
+++ App/dojo.py
@@ -8,11 +8,13 @@ Desc      : Office Allocator model module
 # necessary imports
 # ============================================================================
 import random
-from rooms import Livingspace, Office
+
 from person import Fellow, Staff
 
+from rooms import Livingspace, Office
+
 class Dojo(object):
-    
+
     def __init__(self):
         self.all_rooms = []
         self.fellows = []
@@ -31,11 +33,11 @@ class Dojo(object):
         self.staff = []
         self.allocated_staff = []
 
-    
+
     def create_room(self, name, type_room):
         '''
         Create new rooms for person(s)
-        '''       
+        '''
         room_names = [room.name for room in self.all_rooms]
         msg = ''
         if name in room_names:
@@ -43,23 +45,23 @@ class Dojo(object):
             print(msg)
             return msg
         else:
-        
+
             if type_room.lower() == 'livingspace':
                 new_room = Livingspace(name)
                 self.livingspace.append(new_room)
                 self.all_rooms.append(new_room)
                 msg = ' A Livingspace called %s has been successfully created!' % new_room.name
                 print (msg)
-            
+
             elif type_room.lower() == 'office':
                 new_room = Office(name)
                 self.office.append(new_room)
                 self.all_rooms.append(new_room)
                 msg = ' An office called %s has been successfully created!' % new_room.name
-                print (msg)            
+                print (msg)
             else:
                 print ('invalid')
-            
+
 
     def check_vacant_rooms(self):
         ''' checks for vacant rooms and adds to list'''
@@ -73,7 +75,7 @@ class Dojo(object):
                 if office in self.vacant_offices:
                     self.vacant_offices.remove(office)
                     self.vacant_rooms.remove(office)
-            #  checks for vacant rooms in livingspace       
+            #  checks for vacant rooms in livingspace
         for livingspace in self.livingspace:
             if len(livingspace.members) < livingspace.capacity:
                 if livingspace not in self.vacant_livingspaces:
@@ -83,28 +85,12 @@ class Dojo(object):
                     if livingspace in self.vacant_livingspaces:
                         self.vacant_livingspaces.append(livingspace)
                         self.vacant_rooms.append(livingspace)
-    
+
 
     def add_person(self, name, category, wants_accomodation= 'N'):
         """Add new person"""
         if category == 'fellow':
             new_person = Fellow(name)
-            if self.office:
-                self.check_vacant_rooms()
-                if not self.vacant_offices:
-                    print ( 'There are no rooms available at the moment')
-                    return
-                else:
-                    office_choice = random.choice(self.vacant_offices)
-                    new_person = Fellow(name)
-                    office_choice.members.append(new_person)
-                    self.fellows.append(new_person)
-                    self.all_people.append(new_person)
-                    self.allocated_fellows.append(new_person)
-                    msg = 'Fellow %s has been successfully added and assigned to office %s' % (new_person.name, office_choice.name)
-                    print (msg)
-                    return (msg)
-                print ('There no rooms, please add one by using the create room command')
             if wants_accomodation == 'Y':
                 if self.livingspace:
                     self.check_vacant_rooms()
@@ -113,11 +99,29 @@ class Dojo(object):
                         return
                     else:
                         lspace_choice = random.choice(self.vacant_livingspaces)
-                        new_person = Fellow(name)
+                        # new_person = Fellow(name)
                         lspace_choice.members.append(new_person)
                         self.fellows.append(new_person)
                         self.all_people.append(new_person)
-                        msg = 'Fellow %s has been successfully added and assigned to Livingspace %s' % (new_person.name, lspace_choice.name)
+                        msg = 'Fellow %s has been successfully added and assigned to Livingspace %s !' % (new_person.name, lspace_choice.name)
+                        print (msg)
+                        return (msg)
+                        print '  '
+                    print ('There no rooms, please add one by using the create room command')
+            else:
+                if self.office:
+                    self.check_vacant_rooms()
+                    if not self.vacant_offices:
+                        print ( 'There are no rooms available at the moment')
+                        return
+                    else:
+                        office_choice = random.choice(self.vacant_offices)
+                        # new_person = Fellow(name)
+                        office_choice.members.append(new_person)
+                        self.fellows.append(new_person)
+                        self.all_people.append(new_person)
+                        self.allocated_fellows.append(new_person)
+                        msg = 'Fellow %s has been successfully added and assigned to office %s !' % (new_person.name, office_choice.name)
                         print (msg)
                         return (msg)
                     print ('There no rooms, please add one by using the create room command')
@@ -134,7 +138,7 @@ class Dojo(object):
                     office_choice.members.append(new_person)
                     self.staff.append(new_person)
                     self.all_people.append(new_person)
-                    self.allocated_staff.append(new_person)  
+                    self.allocated_staff.append(new_person)
                     msg = 'Staff %s has been successfully added and assigned to office %s' % (new_person.name, office_choice.name)
                     print (msg)
             else:
@@ -149,18 +153,3 @@ class Dojo(object):
 
     def print_unallocated(self, filename):
         pass
-
-
-            
-
-
-
-
-
-
-
-
-                
-
-
-
